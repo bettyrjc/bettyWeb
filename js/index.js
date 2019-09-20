986146ed47cb98fb6bc462f1b99a3a77060f6d45
bettyrjc <bettyjimenez3010@gmail.com>
2019-09-20 00:20:15 -0400

    cambios

--- js/index.js
+++ js/index.js
@@ -1,15 +1,17 @@
 // primera cosa, accionar
 const clickAction = document.getElementById('toggle_click_burguer');
-const showMenu = document.getElementById('showMenu')
+const showMenu = document.getElementById('showMenu');
+
 clickAction.addEventListener('click', function(){
     console.log('hola');
-    
-    if(showMenu.classList.contains('active')){
-        showMenu.classList.remove('active')
-        console.log('quitando');
-    }else{
-        console.log('añadiendo');
-        showMenu.classList.add('active')
+    showMenu.classList.toggle('active')
+    // if(showMenu.classList.contains('active')){
+    //     showMenu.classList.remove('active')
+    //     console.log('quitando');
+    // }
+    // else{
+    //     console.log('añadiendo');
+    //     showMenu.classList.add('active')
         
-    }
+    // }
 })
